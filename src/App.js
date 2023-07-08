8857bddd676ccbb703f80ca15b479e8712a1f982
microsoftwala <124462509+microsoftwala@users.noreply.github.com>
2023-07-08 08:25:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sponser from "./components/Sponser"
+import About from "./components/About"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Sponser />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
     </div>
