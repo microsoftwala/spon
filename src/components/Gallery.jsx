ce961191f8d7a2fe093a9ef580c81384f0f7539f
microsoftwala <124462509+microsoftwala@users.noreply.github.com>
2023-07-18 11:39:51 +0530

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,6 +3,9 @@ import Box from "@mui/material/Box";
 import Header from './Header';
 import Footer from './Footer';
 import { Typography } from '@mui/material';
+import "../style/gallery.css"
+
+
 
 function Gallery() {
   return (
@@ -41,12 +44,13 @@ function Gallery() {
                     width:"100%"
                   }}
                 >
+        
                   <img
                     alt="png"
                     src="https://images.unsplash.com/photo-1604798756392-b78585747a3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTN8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=600&q=60"
-                    style={{ width:"37%", height:"140px" }}
+                    style={{ width:"37%", height:"140px"}}
                   ></img>
-
+                
                   <img
                     alt="png"
                     src="https://images.unsplash.com/photo-1511688878353-3a2f5be94cd7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
