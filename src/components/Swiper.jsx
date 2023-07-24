e8666955e0e475987c796acdbcfb8d2236555bc0
microsoftwala <124462509+microsoftwala@users.noreply.github.com>
2023-07-24 14:48:31 +0530

    Update Swiper.jsx

--- src/components/Swiper.jsx
+++ src/components/Swiper.jsx
@@ -1,7 +1,7 @@
 // import { useEffect, useRef } from 'react';
 import { register } from 'swiper/element/bundle';
 import Pic1 from "../assets/Gallery_1.png"
-import Pic2 from "../assets/Gallery_2.png"
+import Pic2 from "../assets/Gallery_2.jpg"
 import Pic3 from "../assets/Gallery_3.JPG"
 register();
 
@@ -35,14 +35,14 @@ export default function Swiper() {
         <swiper-slide style={{position:"relative",backgroundColor:"#D9D9D9"}}>
         {/* <img alt="Testing" src='/web-application-testing.png' className='swiper-img'/> */}
                 <div className='absolute bottom-0 mx-[50%] w-[20rem] translate-x-[calc(50%-20rem)] text-center mb-[6rem] mix-blend-darken'>
-                    <img src={Pic1} alt="RR" style={{width:"100%",height:"100%"}}/>
+                    <img src={Pic1} alt="RR" style={{width:"100%"}}/>
                       <h4 className='md:text-lg text-sm'>WELCOME TO RACING PAGE OF IIITDM KANCHEEPURAM</h4>
                       <h1 className='md:text-[3rem] text-[2rem]'>Rebel Racers</h1>
                       <p>Hit the pedal, be the Rebel!</p>
                 </div>
         </swiper-slide>
-        <swiper-slide style={{position:"relative"}}><img src={Pic2} alt="RR" style={{width:"100%",height:"100%"}}/></swiper-slide>
-        <swiper-slide style={{position:"relative"}}><img src={Pic3} alt="RR" style={{width:"100%",height:"100%"}}/></swiper-slide>
+        <swiper-slide style={{position:"relative"}}><img src={Pic2} alt="RR" style={{width:"100%"}}/></swiper-slide>
+        <swiper-slide style={{position:"relative"}}><img src={Pic3} alt="RR" style={{width:"100%"}}/></swiper-slide>
         
       </swiper-container>
     );
